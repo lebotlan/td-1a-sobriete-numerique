206a0236b9d6bd71c8a8b12a20adf06e43c797f5
Didier Le Botlan <lebotlan@users.noreply.github.com>
2023-01-06 12:33:34 +0100

    eqco2 eqcafé

--- Latex/Ada/texte.ads
+++ Latex/Ada/texte.ads
@@ -1,7 +1,7 @@
 package Texte is
    
    -- Renvoie le texte tapé par l'utilisateur
-   -- Utilisation :  Msg : String := Lire_champ_texte ;  (définition)
+   -- Utilisation :  Msg : String := Lire_champ_texte ;  (définition de Msg)
   function Lire_champ_texte return String ;
   
   -- Envoie un message. Il faut indiquer le nombre de destinataires.
